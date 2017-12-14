a4170d1f9ec0c651dbbbcf6223709b463df192b4
Jeff W <jeffwji@gmail.com>
2017-12-13 22:57:58 -0500

    Load message from properties file

--- src/main/java/example/controllers/HelloController.java
+++ src/main/java/example/controllers/HelloController.java
@@ -3,6 +3,7 @@ package example.controllers;
 /**
  *
  */
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +11,7 @@ import java.io.*;
 
 @RestController
 public class HelloController {
-    private String message = "D:/MyPrograms/HelloMicroService/README.md";
+    @Value("${message.location}") private String message;
 
     @RequestMapping("/")
     public String index() throws IOException {
