8bd79a183f405cc4967a8fc2b4db38139dcbf80b
Jeff W <jeffwji@gmail.com>
2017-12-15 15:50:59 -0500

    Add method and document support

--- src/main/java/example/controllers/HelloController.java
+++ src/main/java/example/controllers/HelloController.java
@@ -4,6 +4,7 @@ package example.controllers;
  *
  */
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,7 +14,7 @@ import java.io.*;
 public class HelloController {
     @Value("${message.location}") private String message;
 
-    @RequestMapping("/")
+    @RequestMapping(value="/", method =  RequestMethod.GET )
     public String index() throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(message)));
         return in.readLine();
